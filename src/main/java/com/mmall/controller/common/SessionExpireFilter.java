0e412b1c438edd2d46e58e2dff304f03b72b3666
unknown <759161168@qq.com>
2018-10-14 16:07:20 +0800

    过滤所有.do请求重置session有效期

---
+++ src/main/java/com/mmall/controller/common/SessionExpireFilter.java
@@ -0,0 +1,40 @@
+package com.mmall.controller.common;
+
+
+import com.mmall.common.Const;
+import com.mmall.pojo.User;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JedisPoolUtil;
+import com.mmall.util.JsonUtil;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class SessionExpireFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String token = CookieUtil.readLoginToken(httpServletRequest);
+        if(StringUtils.isNotEmpty(token)){
+            String userJsonStr = JedisPoolUtil.get(token);
+            User user = JsonUtil.string2Obj(userJsonStr,User.class);
+            if(user != null){
+                JedisPoolUtil.expire(token, Const.RedisCacheExtime.REDIS_SESSION_EXTIME);
+            }
+        }
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
